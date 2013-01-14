7502461b5615bba7abe559e8143ea1b8c6a75d46
switer <guankaishe@gmail.com>
2013-01-14 23:39:55 +0800

    settle runing file functions

--- phantomrun.js
+++ phantomrun.js
@@ -1,26 +1,68 @@
-var cp = require('child_process');
-var colors = require('colors');
-var params =  require('./package');
-var htmls = params.config.html;
-var styles = params.config["style"];
-var stylesopts = ' -s ' + styles.join(' -s ');
+var cp = require('child_process')
+var colors = require('colors')
+var params =  require('./package')
+var htmls = params.config.html
+var styles = params.config["style"]
+var stylesopts = ' -s ' + styles.join(' -s ')
 var filePath = 'http://localhost:3013/temp/'
 var filePexi = 'run_result_'
 var dwPath = './deadweight/bin/deadweight'
-var arr = [];
+var args = process.argv
 
-for (var key in htmls) {
-	if ( htmls[key] instanceof Array ) {
-		for (var i = 0 ; i < htmls[key].length; i ++) {
-			arr.push(key + '#' + htmls[key][i]);
-		}
-	} else {
-		arr.push(key);
-	}
+
+var _this = this;
+
+/**
+*	promise pattern 
+*/
+function Promise (done, error) {
+	_this._done = done;
+	_this._error = error;
+
+} 
+Promise.prototype.success = function () {
+	this._done && this._done.apply(this, arguments);
 }
-function startServer (callback) {
-<<<<<<< HEAD
-	var isStart = false;
+
+Promise.prototype.fail = function () {
+	this._error && this._error.apply(this, arguments);
+}
+Promise.prototype.done = function () {
+	_this._done = done;
+}
+
+Promise.prototype.error = function () {
+	_this._error = error;
+}
+
+/**
+*	runing initialize
+**/
+
+function _initialize () {
+	_this.isServerStart = false;
+}
+/**
+*	read file's html propertice
+**/
+function _readHTMLPropertiesAsArray (htmls) {
+	var arr = [];
+	for (var key in htmls) {
+		if ( htmls[key] instanceof Array ) {
+			for (var i = 0 ; i < htmls[key].length; i ++) {
+				arr.push(key + htmls[key][i]);
+			}
+		} else {
+			arr.push(key);
+		}
+	}
+	return arr;
+}
+/**
+*	start a http server for execute deadweight
+**/
+function _startHTTPServer (callback) {
+	var _this._isStart = false;
 	var proc = cp.spawn('node', ["app.js"]);
 	proc.stdout.on('data', function (data) {
 	  console.log('Http Server : server listening on port ' + data);
@@ -39,51 +81,32 @@ function startServer (callback) {
 	proc.on('exit', function (code) {
 	  console.log('child process exited with code ' + code);
 	});
+
+	this._serverPid = proc.pid;
 }
-function doCover(ditHtlms, callback) {
-	console.log('exec : ' + 'deadweight' + stylesopts + ' ' + ditHtlms + ' -o result.log');
-	cp.exec('ruby ' + dwPath + stylesopts + ' ' + ditHtlms + ' -o result.log', function (err, stdout,stderr) {
+/**
+*	Covering...Find verbose selector
+**/
+function _deadweightStyle (styles, htmls) {
+	cp.exec('ruby ' + dwPath + styles + ' ' + htmls + ' -o result.log', function (err, stdout,stderr) {
 		err && console.log(err);
 		console.log(stdout.split('===Error')[0].blue, stdout.split('===Error')[1].red);
-		callback && callback();
+		console.log('Runing server pid : ' + this._serverPid);
 	});
 }
-console.log('exec : ' + 'phantomjs coverhtml.js ' + arr.join(' ').blue);
-=======
-	var proc = cp.spawn('node',['app.js']);
-	var isruning = false;
-	proc.stdout.on('data', function (data) {
-		console.log("HTTP Server : " + data);
-		if (!isruning) {
-			callback && callback();
-			isruning = true;
+
+function _captureHTMLWhithArray (htmls) {
+	console.log('exec : ' + 'phantomjs coverhtml.js ' + htmls.join(' ').blue);
+	cp.exec('phantomjs coverhtml.js ' + htmls.join(' '), function (err, stdout,stderr) {
+		console.log('Parsing URLS : \n' + stdout);
+		var ditHtlms = ' '
+		for (var i = 0; i < htmls.length ; i ++) {
+			ditHtlms += filePath + filePexi + (i + 1) + '.html ';
 		}
-	}),
-	proc.stderr.on('data', function (data) {
-		console.log(data);
+		_startHTTPServer(function () {
+			if (_this.styles) _deadweightStyle(_this.styles, ditHtlms);
+			else throw new Error("Can't find style files !");
+		});
 	});
 }
-function doCover(ditHtlms) {
-	cp.exec('ruby ./deadweight/bin/deadweight' + stylesopts + ' ' + ditHtlms + ' -o result.log', function (err, stdout,stderr) {
-		console.log(err);
-		console.log('stdout : \n' + stdout);
-	});
-}
->>>>>>> d3dd87dd1770b735367465ee9aad9e24b2f5c8d6
-cp.exec('phantomjs coverhtml.js ' + arr.join(' '), function (err, stdout,stderr) {
-	console.log('Parsing URLS : \n' + stdout);
-	var ditHtlms = ' '
-	for (var i = 0; i < arr.length ; i ++) {
-		ditHtlms += filePath + filePexi + (i + 1) + '.html ';
-	}
-<<<<<<< HEAD
-	startServer(function (callback) {
-		doCover(ditHtlms, callback);
-=======
-	startServer(function () {
-		doCover(ditHtlms);
->>>>>>> d3dd87dd1770b735367465ee9aad9e24b2f5c8d6
-	});
-	
-	
-});
+
